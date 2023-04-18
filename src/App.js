0a45747f9bb39daa5c383a12c3a754a3d44e3cf0
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-04-18 20:19:34 -0300

    referencias a los distintos componentes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Portfolio from './components/portfolio/Portfolio'
 import Testimonials from './components/testimonials/Testimonials'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
+import Services from './components/services/Services';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
      <About />
      <Experience />
     <Portfolio />
+    <Services />
     <Testimonials />
     <Contact />
     <Footer />
