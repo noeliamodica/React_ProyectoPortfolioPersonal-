152139f944191bf4b0cc043074f55a12947e3a3b
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-04-19 18:38:33 -0300

    experience component

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import './Experience.css'
+import { BsFillPatchCheckFill } from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id='experience'> Experience </section>
+    <section id='experience'> 
+    <h2> Mi experiencia </h2>
+    <div className='container experience_container'>
+      <h3> Aptitudes </h3>
+      <div className='experience_content'> 
+      <article className='experience_details'> 
+      <BsFillPatchCheckFill />
+      <h4> HTML </h4>
+      </article>
+      <article className='experience_details'> 
+      <BsFillPatchCheckFill />
+      <h4> CSS </h4>
+      </article>
+      <article className='experience_details'> 
+      <BsFillPatchCheckFill />
+      <h4> JavasCript </h4>
+      </article>
+      </div>
+
+    
+    </div>
+    </section>
   )
 }
 
