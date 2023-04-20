bc28a34cf600464eeae2c5902bcf6d297eae3156
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-04-19 21:49:20 -0300

    portfolio component

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -4,7 +4,19 @@ import './Portfolio.css'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'> Portfolio </section>
+    <section id='portfolio'> Portfolio 
+    <h2> Mis proyectos </h2>
+    <div className='container portfolio_container'>
+      <article className='portfolio_item'>
+      <div className='portfolio_item-image'> </div>
+      <h3> Este es un portfolio item titulo </h3>
+      <a href='www.github.com' className='btn' > Github </a>
+      <a href='www.github.com' className='btn btn-primary' > Live demo </a>
+      </article>
+
+    </div>
+
+    </section>
   )
 }
 
