0d0d10e216db9202cc6f97477f2d99104ce12644
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-04-21 20:12:34 -0300

    utilizo swiper

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,4 +1,18 @@
 import React from 'react'
+  // core version + navigation, pagination modules:
+  import Swiper, { Navigation, Pagination } from 'swiper';
+  // import Swiper and modules styles
+  import 'swiper/css';
+  import 'swiper/css/navigation';
+  import 'swiper/css/pagination';
+
+  // init Swiper:
+  const swiper = new Swiper('.swiper', {
+    
+    modules: [Navigation, Pagination],
+  
+  });
+
 
 const Services = () => {
   return (
@@ -6,4 +20,4 @@ const Services = () => {
   )
 }
 
-export default Services
+export default Services 
