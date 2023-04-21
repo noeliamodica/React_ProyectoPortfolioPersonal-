b79c94c1c530594c8c30a26286585cb58e064dbd
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-04-21 20:24:28 -0300

    component services

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,22 +1,32 @@
 import React from 'react'
   // core version + navigation, pagination modules:
-  import Swiper, { Navigation, Pagination } from 'swiper';
+  import { Swiper, SwiperSlide } from 'swiper/react';
   // import Swiper and modules styles
   import 'swiper/css';
-  import 'swiper/css/navigation';
+  
   import 'swiper/css/pagination';
 
-  // init Swiper:
-  const swiper = new Swiper('.swiper', {
-    
-    modules: [Navigation, Pagination],
   
-  });
 
 
 const Services = () => {
   return (
-    <section id='services'> Services </section>
+    <section id='services'> Services 
+
+<Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      ...
+    </Swiper>
+
+    </section>
   )
 }
 
