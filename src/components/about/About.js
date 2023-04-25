283695a3931f8ad204f3ba7e20043d9d17a62cb7
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-04-24 21:55:06 -0300

    about component

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -4,6 +4,11 @@ import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import { FaPython } from 'react-icons/fa'
 import {SiJavascript} from 'react-icons/si'
+import { DiGoogleAnalytics } from 'react-icons/di'
+import { GiWorld } from 'react-icons/gi'
+import { TbReceiptTax } from 'react-icons/tb'
+import { ImCalculator } from 'react-icons/im'
+import { FaBusinessTime } from 'react-icons/fa'
 
 
 const About = () => {
@@ -47,30 +52,30 @@ const About = () => {
                            <h5> Coderhouse </h5>
                         </div>
                         <div className="item">
-                         
+                         <DiGoogleAnalytics className='item-icon'/>
                         <h3> Data analytics </h3>
                            <h5> Coderhouse </h5>
                         </div>
                         <div className="item">
-                           
+                        <GiWorld className='item-icon'/>
                         <h3> Esp en Tributacion Internacional </h3>
                            <h5> Universidad Complutense de Madrid - UBA</h5>
                         </div>
                         
                         <div className="item">
-                           
+                           <TbReceiptTax className='item-icon'/>
                            <h3> Especialista en Tributacion</h3>
                               <h5> Universidad Nacional de Salta </h5>
                            </div>
                         
                         <div className="item">
-                           
+                           <ImCalculator className='item-icon'/>
                            <h3> Contadora Publica </h3>
                           <h5> Universidad Catolica de Salta </h5>
                         </div>
 
                         <div className="item">
-                           
+                           <FaBusinessTime className='item-icon'/>
                         <h3> Lic. en Administracion de Empresas </h3>
                            <h5>  Universidad Catolica de Salta</h5>
                         </div>
