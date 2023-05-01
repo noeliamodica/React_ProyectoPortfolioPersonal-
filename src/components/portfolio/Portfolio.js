8b36b5c62b9c6c2d433574989778324f57754696
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-05-01 16:56:28 -0300

    cambios en Portfolio

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,24 +1,38 @@
-import React from 'react'
+
+import { useState, useEffect } from "react";
 import './Portfolio.css'
+import axios from "axios";
 
 
 const Portfolio = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios("db.json").then((res) => setData(res.data));
+  }, []);
+
+  return data.map((projects) => {
+
   return (
     <section id='portfolio'> Portfolio 
     <h2> Mis proyectos </h2>
     <div className='container portfolio_container'>
       <article className='portfolio_item'>
+      <img src={projects.img} alt='img-product-card' />
+        <h3>{projects.nombre}</h3>
       <div className='portfolio_item-image'> </div>
       <h3> Este es un portfolio item titulo </h3>
       <a href='www.github.com' className='btn' > Github </a>
       <a href='www.github.com' className='btn btn-primary' > Live demo </a>
+      
       </article>
 
     </div>
 
     </section>
-  )
-}
+  );
+});
+};
 
 export default Portfolio
 
