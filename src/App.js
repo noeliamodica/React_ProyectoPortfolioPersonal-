cee7c7555f4cfe735ce3222387005e76a175c79b
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-05-01 17:49:49 -0300

    trabajo con imagenes del db

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Nav from './components/nav/Nav'
 import About from './components/about/About'
 import Experience from './components/experience/Experience'
 import Portfolio from './components/portfolio/Portfolio'
-
 import Contact from './components/contact/Contact'
 
 
--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,37 +1,31 @@
 
-import { useState, useEffect } from "react";
 import './Portfolio.css'
-import axios from "axios";
+import projects from './db.json'
 
 
 const Portfolio = () => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    axios("db.json").then((res) => setData(res.data));
-  }, []);
-
-  return data.map((projects) => {
+  console.log(projects)
 
   return (
-    <section id='portfolio'> Portfolio 
-    <h2> Mis proyectos </h2>
-    <div className='container portfolio_container'>
-      <article className='portfolio_item'>
-      <img src={projects.img} alt='img-product-card' />
-        <h3>{projects.nombre}</h3>
-      <div className='portfolio_item-image'> </div>
-      <h3> Este es un portfolio item titulo </h3>
-      <a href='www.github.com' className='btn' > Github </a>
-      <a href='www.github.com' className='btn btn-primary' > Live demo </a>
-      
-      </article>
+    <div>
 
-    </div>
+        {projects.map((project) => (
+                        <div className='logo-contenedor'>
+                        <img
+                        src={project.imagen}
+                        className='imagen'
+                        alt={project.nombre} />
+                        <div>
+                            {project.nombre}
+                        </div>
+                  </div>
 
-    </section>
+                            ))
+                        }
+                        
+                
+        </div>
   );
-});
 };
 
 export default Portfolio
