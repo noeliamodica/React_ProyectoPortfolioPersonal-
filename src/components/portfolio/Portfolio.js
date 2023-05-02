7edd7cfce955bf2db2d0e684bf75fcf5dcd23c7b
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-05-02 11:14:38 -0300

    css portfolio

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,16 +1,39 @@
 import './Portfolio.css'
 import projects from './db.json'
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 
 const Portfolio = () => {
   console.log(projects)
 
+  const responsive = {
+    superLargeDesktop: {
+       // the naming can be any, depends on you.
+       breakpoint: { max: 4000, min: 3000 },
+       items: 5,
+    },
+    desktop: {
+       breakpoint: { max: 3000, min: 1024 },
+       items: 3,
+    },
+    tablet: {
+       breakpoint: { max: 1024, min: 464 },
+       items: 2,
+    },
+    mobile: {
+       breakpoint: { max: 464, min: 0 },
+       items: 1,
+    },
+ };
+
   return (
     <section id='portfolio'>
       <h2> Mis proyectos </h2>
     <div className='portfolio_container'> 
+    <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
       {projects.map((project) => (
-        <div>
+        <div className='item-p'>
         <div>
         {project.nombre}
         </div>
@@ -25,11 +48,12 @@ const Portfolio = () => {
         <button>
         {project.github}
         </button>
+        
        </div>
 
          ))
         }
-                        
+      </Carousel>                
                 
     </div>
     </section>
